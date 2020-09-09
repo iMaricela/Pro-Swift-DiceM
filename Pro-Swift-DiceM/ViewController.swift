1ab7ac97eb865c4c5d6fcca38fce03a8345312ab
iMaricela <44278837+iMaricela@users.noreply.github.com>
2020-09-09 19:03:36 +0200

    Initial Commit

---
+++ Pro-Swift-DiceM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pro-Swift-DiceM
+//
+//  Created by Marzieh Sharafi on 9/9/20.
+//  Copyright © 2020 Marzieh Sharafi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
